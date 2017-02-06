7db7a3ee70a21733d606967c01e4561fddba749b
Paul Dobson <paul.d.dobson@gmail.com>
2017-02-06 21:15:34 +0000

    simple

--- simple.py
+++ simple.py
@@ -12,4 +12,4 @@ def index():
 # Run the app.
 if __name__ == '__main__':
 	#app.run(debug=True, port=5000)
-	app.run(host='0.0.0.0',debug=False, port=80)
+	app.run(host='0.0.0.0',debug=False, port=5000)
