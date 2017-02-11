51b2df05e20257f7580b5e2f96c79dea9f0590b3
Paul Dobson <paul.d.dobson@gmail.com>
2017-02-11 00:21:34 +0000

    Notes on SSL

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 source activate cheminformatics
 
+#http://werkzeug.pocoo.org/docs/0.10/serving/
+#http://stackoverflow.com/questions/7340784/easy-install-pyopenssl-error
+
 python empathy_socket.py &
 python api_external.py &
 python landing.py $1 &
