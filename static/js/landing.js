800ea0402eebbb800cff6f7653c2e206991dd699
Paul Dobson <paul.d.dobson@gmail.com>
2017-03-02 10:13:57 +0000

    Change port

--- static/js/landing.js
+++ static/js/landing.js
@@ -7,7 +7,7 @@ landingApp.controller('landingCtrl', ['$scope', '$http', '$rootScope', '$window'
 	//$scope.static_url = 'localhost:8080';
 	//$scope.socket_static_url = 'localhost:8082';
 	//$scope.external_static_url = 'localhost:8081';
-	$scope.static_url = 'www.metabolicjamboree.co.uk:8080';
+	$scope.static_url = 'www.metabolicjamboree.co.uk:80';
 	$scope.socket_static_url = 'www.metabolicjamboree.co.uk:8082';
 	$scope.external_static_url = '35.187.33.1:8081';
 
