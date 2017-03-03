b39f34c47543d79309c631f7b093ca77787a7c36
Paul Dobson <paul.d.dobson@gmail.com>
2017-03-03 12:35:54 +0000

    Startup process

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#Jamboree app startup script
+
+echo 'Reroute IP tables'
+sudo /sbin/iptables -t nat -I PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 8080
+
+echo 'Launch ancillary services'
+python empathy_ancillary.py &
+
+echo 'Launch Socket'
+python empathy_socket.py &
+
+echo 'Launch core'
+python landing.py $1 &
