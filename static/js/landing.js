71b264edcbf98edfb3fdfdc878c8dffad611734a
Paul Dobson <paul.d.dobson@gmail.com>
2017-05-10 11:28:24 +0100

    Logging Json wrapper

--- static/js/landing.js
+++ static/js/landing.js
@@ -736,7 +736,8 @@ landingApp.controller('landingCtrl', ['$scope', '$http', '$rootScope', '$window'
 	//Update node property
 	$scope.updateText = function(key,value) {
 		console.log('Update text', $scope.selection, 'with', key, ':', value);
-		if(key == 'Notes') {		//Wrap notes in JSON (unwrapper expects this)
+		if(key === 'Notes') {		//Wrap notes in JSON (unwrapper expects this)
+			console.log('Update text wrapping to JSON');
 			value = angular.toJson(value);
 			}
 		bundle = {"port": $scope.port, "record_handle": $scope.record_handle, "id":$scope.selection, "key":key, "value":value};
