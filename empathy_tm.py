c7b09e0d9409ce4072fe872e69e33af33dc6785a
Paul Dobson <paul.d.dobson@gmail.com>
2017-05-19 14:45:33 +0100

    TM ticker

--- empathy_tm.py
+++ empathy_tm.py
@@ -27,7 +27,7 @@ def processCalliope(hits):
 			processed['pmid'] = hit["_source"]["MedlineCitation"]["pmid"]["content"]
 			processed['highlight'] = json.dumps(highlight)
 			results.append(json.dumps (processed) ) #Stored in database as list of JSON 
-	print 'Calliope hits', len(hits)
+	print 'Calliope hits', len(results)
 	return results
 
 #Calliope query
